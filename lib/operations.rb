f8b9e882667260dd328e9b808d90550327485dd0
Sherief Farid <sherief@farid.cloud>
2019-02-10 04:18:21 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,16 @@
 def unsafe?(speed)
 
+if (speed < 40 || speed > 60)
+
+  return true
+end
+return false
 end
 
 
 
 def not_safe?(speed)
-	
+
+speed < 40 || speed > 60 ? true : false
+
 end
-	
-
-
